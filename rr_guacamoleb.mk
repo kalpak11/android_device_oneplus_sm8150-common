e7f1c9e206c602d13265bd8a2d2bcd06c32e5d55
kalpak11 <patelkalpak11@gmail.com>
2019-12-24 00:55:06 +0530

    op7: rr bringup

---
+++ rr_guacamoleb.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from guacamole device
+$(call inherit-product, device/oneplus/guacamoleb/device.mk)
+
+# Inherit some common AOSiP stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := rr_guacamoleb
+PRODUCT_DEVICE := guacamoleb
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := OnePlus 7
+PRODUCT_MANUFACTURER := OnePlus
+
+PRODUCT_AAPT_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+# Build info
+BUILD_FINGERPRINT := "OnePlus/OnePlus7/OnePlus7:10/QKQ1.190716.003/1911140100:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=OnePlus7 \
+    PRODUCT_NAME=OnePlus7 \
+    PRIVATE_BUILD_DESC="OnePlus7-user 10 QKQ1.190716.003 1911140100 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
